e708fa9c02e75bf2377efe4db2c40d0562d7a9d2
wolfieorama <wachirawamunyi@gmail.com>
2018-06-12 22:25:20 +0200

    added product class

---
+++ product.rb
@@ -0,0 +1,9 @@
+class Product
+  attr_reader :product_code, :name, :price
+
+  def initialize(product_code, name, price)
+    @product_code = product_code
+    @name = name
+    @price = price
+  end
+end
