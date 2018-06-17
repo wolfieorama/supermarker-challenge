ae0cff6c809893af0e29670caa99ba24a6ffa4af
wolfieorama <wachirawamunyi@gmail.com>
2018-06-17 11:39:17 +0200

    added discount service class to cater for discouting logic

--- lib/discounting_service.rb
+++ lib/discounting_service.rb
@@ -1,2 +1,26 @@
 class DiscountingService
+
+  def initialize(rules = [])
+    @rules = rules
+  end
+
+  def price_used(product, count)
+    new_price = nil
+
+    rules.each do |rule|
+      if rule.discount__rule_type = Rule::DISCOUNT_RULE_TYPES[:two_or_more]
+        new_price = rule.apply_discount(product_code, product_count)
+      end
+    end
+
+    if new_price.nil?
+      product.price
+    else
+      new_price
+    end
+  end
+
+  private
+
+  attr_reader :rules
 end
